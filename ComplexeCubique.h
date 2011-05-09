2db5c9943a348a593e5d18bbfb65c704179167c0
frossigneux <francois.rossigneux@gmail.com>
2011-05-09 11:29:47 -0700

    Complexe cubique

--- ComplexeCubique.h
+++ ComplexeCubique.h
@@ -8,15 +8,38 @@ class ComplexeCubique
 {
 	public:
 		bool estValide();
-		Cellule::iterator trouverCellule(Cellule*);
-		Cellule::iterator trouverIPlus1Cellule(Cellule*);
-		bool estDansBord(Cellule*, Cellule*);
-		void creeBord(Cellule*, 2i pointeurs);
+		/*VirtualCellule::iterator trouverCellule(VirtualCellule*);
+		VirtualCellule::iterator trouverIPlus1Cellule(VirtualCellule*);
+		bool estDansBord(VirtualCellule*, VirtualCellule*);
+		void creeBord(VirtualCellule*, 2i pointeurs);
 		Cellule* creerCellule(Point);
-		void detruire(Cellule *);
-		void reduction(Cellule*, Cellule*);
+		void detruire(VirtualCellule *);
+		void reduction(VirtualCellule*, Cellule*);*/
 	private:
-		vector<Cellule> ensemblesCellules;//TODO virtual cellule
+		VirtualCellule* ensemblesCellules[n];
 };
+
+template<int n, int k, class T>
+bool ComplexeCubique<n,k,T>::estValide()
+{
+	//Vérifie que chaque i-cellule possède bien 2*i (i-1)-cellules dans son bord (des pointeurs non NULL)
+	if(n == 0)
+	{
+		return true;
+	}
+	else
+	{
+		for(int i = 1; i <= n; i++)
+		{
+			Cellule<i,k,T>* cellule = this->ensemblesCellules[i];
+			for(int j = 0; j < 2*i; j++)
+			{
+				if(cellule->getBord()[j] == NULL)
+					return false;
+			}
+		}
+		return true;
+	}
+}
 
 #endif
