cf92c444f9843712ee68ad21ede37b1e76a3b77e
Nicolas Useldinger <quatrevingtdeux@gmail.com>
2011-05-09 12:10:56 +0200

    Test

--- Point.h
+++ Point.h
@@ -1,8 +1,15 @@
 #ifndef POINT_H
 #define POINT_H
 
+template <int k, class T>
 class Point
 {
+	public:
+		Point();
+		setCoordonnees();
+		
+	private:
+		std::vector<T> coordonnees; //TODO taille k
 };
 
 #endif
