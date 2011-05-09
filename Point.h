1efc33e900c00e0bf9a7b8125679680546821ef0
Nicolas Useldinger <quatrevingtdeux@gmail.com>
2011-05-09 17:50:31 +0200

    Revert "Test"
    This reverts commit cf92c444f9843712ee68ad21ede37b1e76a3b77e.

--- Point.h
+++ Point.h
@@ -1,15 +1,8 @@
 #ifndef POINT_H
 #define POINT_H
 
-template <int k, class T>
 class Point
 {
-	public:
-		Point();
-		setCoordonnees();
-		
-	private:
-		std::vector<T> coordonnees; //TODO taille k
 };
 
 #endif
