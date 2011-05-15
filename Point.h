372e65c283699d8fc21fe8245d10837d370c7a63
François Rossigneux <francois.rossigneux@gmail.com>
2011-05-15 21:26:52 +0200

    Validation de Point.h (plus de bugs a priori)

--- Point.h
+++ Point.h
@@ -2,38 +2,50 @@
 #define POINT_H
 
 #include <iostream>
+#include <vector>
 #include <assert.h>
+
+// ============================================================
+// Classe Point
+// ============================================================
 
 template <int k, typename T>
 class Point
 {
-	public:
-		Point();
-		Point(T coord[k]);
-		~Point() {}
-		int getDimension();
-		T& operator[](int i);
+        public:
+                Point(); // Le constructeur n'initialise pas le tableau de coordonnées (impossible car T varie)
+                Point(const std::vector<T> coord);
+                ~Point() {} // Le destructeur ne détruit rien (car T peut être un int, par exemple)
+                int getDimension() const;
+                T& operator[](int i);
+                template<int _k, typename _T> friend std::ostream& operator<<(std::ostream& sortie, const Point<_k,_T>& point);
 		
 	private:
-		T coordonnees[k];
+                T coordonnees[k];
 };
+
+// ============================================================
+// Implémentation des méthodes
+// ============================================================
 
 template <int k, typename T>
 Point<k,T>::Point()
 {
-	assert(k > 0);
+    assert(k > 0);
 }
 
 template <int k, typename T>
-Point<k,T>::Point(T coord[k])
+Point<k,T>::Point(const std::vector<T> coord)
 {
-	assert(k > 0);
-	for(int i = 0; i < k; i++)
-		coordonnees[i] = coord[i]; 
+        assert(k > 0 && coord.size() == k);
+        for(int i = 0; i < k; i++)
+        {
+            coordonnees[i] = coord[i];
+        }
 }
 
 template <int k, typename T>
-int Point<k,T>::getDimension()
+int Point<k,T>::getDimension() const
 {
 	return k;
 }
@@ -43,6 +55,17 @@ T& Point<k,T>::operator[](int i)
 {
 	assert(i >= 0 && i < k);
 	return coordonnees[i];
+}
+
+template <int k, typename T>
+std::ostream& operator<<(std::ostream& sortie, const Point<k,T>& point)
+{
+    sortie << "Point :" << std::endl;
+    sortie << "\tDimension = " << point.getDimension() << std::endl;
+    sortie << "\tAdresse = " << &point << std::endl;
+    for(int i=0; i<k; i++)
+        sortie << "\tcoordonnees[" << i << "] = " << point.coordonnees[i] << std::endl;
+    return sortie;
 }
 
 #endif
